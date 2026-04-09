e19172da0afc32c2140b555684bc4870e1d3acbd
CrowOfMinerva <reledia@protonmail.com>
2026-04-09 16:48:10 +0300

    Refactoring config.go

--- config/config.go
+++ config/config.go
@@ -8,7 +8,6 @@ import (
 	"github.com/knadh/koanf/providers/file"
 	"github.com/knadh/koanf/providers/structs"
 	"github.com/knadh/koanf/v2"
-	"github.com/rs/zerolog/log"
 )
 
 var (
@@ -16,6 +15,15 @@ var (
 	parser  = toml.Parser()
 	home, _ = os.LookupEnv("HOME")
 	path    = filepath.Join(home, ".config/gumu/config.toml")
+
+	defaultConfig = Config{
+		ConfigProton: ConfigProton{
+			Repos: []string{"cachyos/proton-cachyos", "GloriousEggroll/proton-ge-custom"},
+		},
+		ConfigPrefix: ConfigPrefix{
+			Prefixs: []string{""},
+		},
+	}
 )
 
 type ConfigProton struct {
@@ -36,58 +44,28 @@ func LoadConf() (*Config, error) {
 
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		os.MkdirAll(filepath.Dir(path), 0o755)
-		f, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0o600)
-
-		log.Err(err).Send()
-
-		config = Config{
-			ConfigProton: ConfigProton{
-				Repos: []string{"cachyos/proton-cachyos"},
-			},
-			ConfigPrefix: ConfigPrefix{
-				Prefixs: []string{""},
-			},
-		}
-
-		err = k.Load(structs.Provider(config, "koanf"), nil)
-		if err != nil {
-			log.Error().Err(err).Send()
-			return &config, err
-		}
-		bytes, err := k.Marshal(parser)
-		if err != nil {
-			log.Error().Err(err).Send()
-			return &config, err
-		}
-		_, err = f.Write(bytes)
-		if err != nil {
-			log.Error().Err(err).Send()
-			return &config, err
-		}
-	} else {
-		if err := k.Load(file.Provider(path), parser); err != nil {
-			return &config, err
-		}
-		k.UnmarshalWithConf("", &config, koanf.UnmarshalConf{Tag: "koanf"})
+		return &defaultConfig, defaultConfig.Save()
 	}
+
+	if err := k.Load(file.Provider(path), parser); err != nil {
+		return &config, err
+	}
+	k.UnmarshalWithConf("", &config, koanf.UnmarshalConf{Tag: "koanf"})
 	return &config, nil
 }
 
 func (c *Config) Save() error {
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_TRUNC, 0o755)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_TRUNC|os.O_CREATE, 0o644)
 	if err != nil {
-		log.Error().Err(err).Send()
+		return err
 	}
 	f.Truncate(0)
 	kk := koanf.New(".")
 	kk.Load(structs.Provider(c, "koanf"), nil)
 	bytes, err := kk.Marshal(parser)
 	if err != nil {
-		log.Error().Err(err).Send()
+		return err
 	}
 	_, err = f.WriteAt(bytes, 0)
-	if err != nil {
-		log.Error().Err(err).Send()
-	}
 	return err
 }
